1ec61eebb74bbdd1c49e8604dae0ab08427082b5
Yusuke Arai <yusuke@mbp-txtbokwrm.local>
2018-05-20 15:33:38 +0900

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'pages#index'
   get 'pages/index'
   get 'pages/show'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
