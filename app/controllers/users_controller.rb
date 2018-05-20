9a34527eddf04f8f2f4760345be3f5d887cc68e9
Yusuke Arai <yusuke@mbp-txtbokwrm.local>
2018-05-20 11:21:31 +0900

    form completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+    @user.addresses.build
   end
 
   # GET /users/1/edit
@@ -69,6 +70,10 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :age)
+      params.require(:user).permit(
+        :username,
+        :age,
+        addresses_attributes: [:id, :zipcode, :city, :street, :tel]
+      )
     end
 end
