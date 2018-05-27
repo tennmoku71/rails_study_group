52bd1bfebadc37f04d47ff55eea88ff747990bec
Yusuke Arai <yusuke@mbp-txtbokwrm.local>
2018-05-27 15:47:54 +0900

    calendar complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require moment
 //= require activestorage
 //= require turbolinks
+//= require fullcalendar
+//= require rails-ujs
 //= require_tree .
